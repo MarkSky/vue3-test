f510c70c58dd03853aeffc7227c4abd2494f3601
MarkSky <rdmark@outlook.com>
2025-02-03 18:06:20 +0800

    [Setting] Vite 設定檔

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import VueRouter from 'unplugin-vue-router/vite';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        VueRouter(),
+        vue(),
+        VueI18nPlugin({
+            include: path.resolve(__dirname, 'src/locales/**'),
+        }),
+        vueDevTools(),
+        basicSsl(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
-})
+    server: {
+        host: true,
+    },
+});
