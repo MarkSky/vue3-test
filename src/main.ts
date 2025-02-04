b50847c318cfcbeefdfe017f57c4f008331145bc
MarkSky <rdmark@outlook.com>
2025-02-04 17:31:11 +0800

    [Update] 調整Pinia架構

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 
 import App from './App.vue';
 import router from './router';
 import i18n from './plugins/i18n';
+import pinia from './stores';
 
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(pinia);
 app.use(router);
 app.use(i18n);
 
