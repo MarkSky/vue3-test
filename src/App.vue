83674b30558637a802d922f4b322c6d14c17ee65
MarkSky <rdmark@outlook.com>
2025-02-06 17:47:10 +0800

    [Update] 加上錯誤邊界

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,27 @@
     */
     // Components
     import CSSRuntimeProvider from '@master/css.vue';
+    import ErrorBoundary from '@/components/ErrorBoundary.vue';
     // Styles
     import masterCSSConfig from '../master.css';
+
+    // function
+    const getTransitionName = (transition: unknown): string => {
+        return typeof transition === 'string' ? transition : 'default';
+    };
 </script>
 
 <template>
-    <CSSRuntimeProvider :config="masterCSSConfig">
-        <RouterView />
-    </CSSRuntimeProvider>
+    <ErrorBoundary>
+        <CSSRuntimeProvider :config="masterCSSConfig">
+            <RouterView v-slot="{ Component, route }">
+                <Transition :name="getTransitionName(route.meta.transition)">
+                    <component :is="Component" />
+                </Transition>
+                <!-- <Transition name="default">
+                <component :is="Component" />
+            </Transition> -->
+            </RouterView>
+        </CSSRuntimeProvider>
+    </ErrorBoundary>
 </template>
