0af91a9e4ce6e7a878c93e2c7d0f89b073ea0ec6
MarkSky <rdmark@outlook.com>
2025-02-13 18:00:55 +0800

    [Update] 加入Layout元件顯示

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,11 @@
        import
     */
     // Components
+    import type { Component } from 'vue';
     import CSSRuntimeProvider from '@master/css.vue';
     import ErrorBoundary from '@/components/ErrorBoundary.vue';
+    // Utils
+    import layoutMapping from './utils/layoutMapping';
     // Styles
     import masterCSSConfig from '../master.css';
 
@@ -12,18 +15,24 @@
     const getTransitionName = (transition: unknown): string => {
         return typeof transition === 'string' ? transition : 'default';
     };
+
+    // function
+    const getLayoutName = (layout: unknown): Component => {
+        const layoutKey = typeof layout === 'string' ? layout : 'default';
+
+        return layoutMapping[layoutKey] || layoutMapping['default'];
+    };
 </script>
 
 <template>
     <ErrorBoundary>
         <CSSRuntimeProvider :config="masterCSSConfig">
             <RouterView v-slot="{ Component, route }">
-                <Transition :name="getTransitionName(route.meta.transition)">
-                    <component :is="Component" />
-                </Transition>
-                <!-- <Transition name="default">
-                <component :is="Component" />
-            </Transition> -->
+                <component :is="getLayoutName(route.meta.layout)">
+                    <Transition :name="getTransitionName(route.meta.transition)">
+                        <component :is="Component" />
+                    </Transition>
+                </component>
             </RouterView>
         </CSSRuntimeProvider>
     </ErrorBoundary>
